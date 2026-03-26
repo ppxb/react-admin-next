f4c8fd8d922a218ebe0d004f5d56c19dcf2dbf05
ppxb <317842449@qq.com>
2026-03-26 11:05:06 +0800

    chore: use vite default minify

--- plugins/compression.ts
+++
@@ -1,9 +0,0 @@
-import { compression } from 'vite-plugin-compression2'
-
-export default function createCompressionPlugin() {
-  return compression({
-    algorithms: ['gzip'],
-    threshold: 10240,
-    deleteOriginalAssets: false,
-  })
-}
